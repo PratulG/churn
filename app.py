caba201ebfe60129da80e1661837eada7d74bf8e
Pratul Goyal <39401239+PratulG@users.noreply.github.com>
2023-06-10 14:10:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import cloudpickle as pickle
 
 # Load the trained RandomForestClassifier model
 with open('best_model.pkl', 'rb') as file:
